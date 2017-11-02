85d471b94470c56803443bef3f8e9ac58c4ee1e6
Chance Smith <chancesmithb@gmail.com>
2017-11-01 20:41:13 -0500

    wraps email sending in a function

--- send_email.py
+++ send_email.py
@@ -11,43 +11,52 @@ import smtplib
 # Jinja2 templating
 from jinja2 import Environment
 
-# vars
-email_send_to = 'chance@sodiumhalogen.com'
+def send_email(email_send_to, gif_path, email_firstName, email_lastName):
 
-# Import the email modules we'll need
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+  # Import the email modules we'll need
+  from email.mime.image import MIMEImage
+  from email.mime.multipart import MIMEMultipart
+  from email.mime.text import MIMEText
 
-msg = MIMEMultipart()
+  msg = MIMEMultipart()
 
-# Open a plain text file for reading.  For this example, assume that
-# the text file contains only ASCII characters.
-textfile = 'email-template.html'
-with open(textfile, 'rb') as fp:
-  # Create a text/plain message
-  msg.attach( MIMEText(Environment().from_string(fp.read()).render(
-      firstName="sam",
-      lastName="smith"
-    ), 'html')
-  )
+  # Open a plain text file for reading.  For this example, assume that
+  # the text file contains only ASCII characters.
+  textfile = 'email-template.html'
+  with open(textfile, 'rb') as fp:
+    # Create a text/plain message
+    msg.attach( MIMEText(Environment().from_string(fp.read()).render(
+        firstName=email_firstName,
+        lastName=email_lastName
+      ), 'html')
+    )
 
-# add gif
-# Open the files in binary mode.  Let the MIMEImage class automatically
-# guess the specific image type.
-gif = 'files/1aff3698-7e47-4f19-9b19-ef40a7c503a5.gif'
-with open(gif, 'rb') as fp:
-  msg.attach( MIMEImage(fp.read()) )
+  # attach gif
+  # Open the files in binary mode.  Let the MIMEImage class automatically
+  # guess the specific image type.
+  with open(gif_path, 'rb') as fp:
+    msg.attach( MIMEImage(fp.read()) )
 
-# email headers
-msg['Subject'] = 'Your gif is ready'
-msg['From'] = email
-msg['To'] = email_send_to
+  # email headers
+  msg['Subject'] = 'Your gif is ready'
+  msg['From'] = email
+  msg['To'] = email_send_to
 
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-s = smtplib.SMTP(server,port)
-s.starttls()
-s.login(email,password)
-s.sendmail(email, email_send_to, msg.as_string())
-s.quit()
+  # Send the message via our own SMTP server, but don't include the
+  # envelope header.
+  s = smtplib.SMTP(server,port)
+  s.starttls()
+  s.login(email,password)
+  ## Send message
+  try:
+    s.sendmail(email, email_send_to, msg.as_string())
+    return True
+  except Exception: # try to avoid catching Exception unless you have too
+    return False
+  finally:
+    s.quit()
+
+if send_email('chance@sodiumhalogen.com','files/1aff3698-7e47-4f19-9b19-ef40a7c503a5.gif','Tiny','Turner') is True:
+  print "Email sent"
+else:
+  print "No dice. Email sending failed."
